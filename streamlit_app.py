7514f65f27da0fae6f202f13c263d782b9bbf3f7
Nicolas Wender <nicolas.w3nder@gmail.com>
2025-03-16 17:46:02 -0300

    carregando e limpando base de dados

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,15 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="Car Sales Analysis Dashboard", page_icon="🚗")
+
+# carregando e limpando base de dados
+df = pd.read_csv("db/car_sales.csv") 
+
+df["Date"] = pd.to_datetime(df["Date"], errors="coerce")
+df["Engine"] = df["Engine"].str.replace(r"[^\w\s]", "", regex=True)
+
+df["Phone"] = df["Phone"].astype(str)
+df["Dealer_No"] = df["Dealer_No"].astype(str)
+
+text_columns = df.select_dtypes(include=["object"]).columns
+df[text_columns] = df[text_columns].apply(lambda x: x.str.strip())
